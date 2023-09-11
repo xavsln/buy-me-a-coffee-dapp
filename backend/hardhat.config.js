890d8922e9681acfad80077e45f6d0d80ea8e7d1
xavsln <19362582+xavsln@users.noreply.github.com>
2023-09-11 11:38:04 +0200

    Create deploy.js to deploy on Sepolia

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+// console.log(process.env.TEST_DOTENV);
+
+const SEPOLIA_URL = process.env.ALCHEMY_SEPOLIA_URL;
+const TEST_PRIVATE_KEY = process.env.TEST_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [TEST_PRIVATE_KEY],
+    },
+  },
 };
