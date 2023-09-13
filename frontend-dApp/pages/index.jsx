068a93c6d62f6b951590e9ad210549d81fe43d64
xavsln <19362582+xavsln@users.noreply.github.com>
2023-09-13 11:52:27 +0200

    Adapt frontend to interact with deployed Contract

--- frontend-dApp/pages/index.jsx
+++ frontend-dApp/pages/index.jsx
@@ -6,7 +6,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   // Contract Address & ABI
-  const contractAddress = "0xDBa03676a2fBb6711CB652beF5B7416A53c1421D";
+  const contractAddress = "0x323C1787E850F67f942d4367F64864a0A6B6271d";
   const contractABI = abi.abi;
 
   // Component state
@@ -236,7 +236,7 @@ export default function Home() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Created by @thatguyintech for Alchemy's Road to Web3 lesson two!
+          Based on Alchemy's Road to Web3 lesson two!
         </a>
       </footer>
     </div>
